95f1296709eadfef7a416a9e1aa30a20b935be60
Aaron Edwards <aaron.j.edwards@me.com>
2016-11-02 16:31:51 -0700

    Added main sign in screen and first user registration screen.

--- MacroManager/MacroManager/RegistrationViewController.swift
+++ MacroManager/MacroManager/RegistrationViewController.swift
@@ -1,32 +1,45 @@
 //
-//  SignInViewController.swift
+//  RegistrationViewController.swift
 //  MacroManager
 //
-//  Created by Capstone Crew on 10/31/16.
-//  Copyright © 2016 Capstone Crew. All rights reserved.
+//  Created by Aaron James Edwards on 11/1/16.
+//  Copyright © 2016 Aaron Edwards. All rights reserved.
 //
 
 import UIKit
 
-class SignInViewController: UIViewController, UITextFieldDelegate{
+class RegistrationViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var createAccountButton: UIButton!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //SETS STATUS BAR TO WHITE - AJE
         UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
         
         //CONFIGURE TEXT BOXES - AJE
+        nameField.attributedPlaceholder = NSAttributedString(string:"NAME",
+                                                              attributes:[NSForegroundColorAttributeName: UIColor.white])
+        nameField.borderStyle = .none
+        nameField.textColor = UIColor.white
+        nameField.tag = 0
+        nameField.delegate = self
+        nameField.returnKeyType = .next
+        nameField.keyboardType = .default
+        nameField.autocapitalizationType = .words
+        nameField.autocorrectionType = .no
+        nameField.tintColor = UIColor.white
+        
         emailField.attributedPlaceholder = NSAttributedString(string:"EMAIL",
-                                                                 attributes:[NSForegroundColorAttributeName: UIColor.white])
+                                                              attributes:[NSForegroundColorAttributeName: UIColor.white])
         emailField.borderStyle = .none
         emailField.textColor = UIColor.white
-        emailField.tag = 0
+        emailField.tag = 1
         emailField.delegate = self
         emailField.returnKeyType = .next
         emailField.keyboardType = .emailAddress
@@ -38,9 +51,9 @@ class SignInViewController: UIViewController, UITextFieldDelegate{
                                                                  attributes:[NSForegroundColorAttributeName: UIColor.white])
         passwordField.borderStyle = .none
         passwordField.textColor = UIColor.white
-        passwordField.tag = 1
+        passwordField.tag = 2
         passwordField.delegate = self
-        passwordField.returnKeyType = .done
+        passwordField.returnKeyType = .continue
         passwordField.isSecureTextEntry = true
         passwordField.keyboardType = .default
         passwordField.autocapitalizationType = .none
@@ -49,22 +62,21 @@ class SignInViewController: UIViewController, UITextFieldDelegate{
         //GESTURE RECOGNIZER TO HIDE KEYBOARD - AJE
         let tap = UITapGestureRecognizer(target: self, action: #selector(RegistrationViewController.hideKeyboard))
         view.addGestureRecognizer(tap)
-
         
         //CONFIGURE LOGIN BUTTON - AJE
-        loginButton.backgroundColor = UIColor.white
-        loginButton.layer.cornerRadius = 5
+        nextButton.backgroundColor = UIColor.white
+        nextButton.layer.cornerRadius = 5
         
         // Do any additional setup after loading the view.
+
     }
     
     //HIDE KEYBOARD WHEN TOUCHING ANYWHERE ON SCREEN - AJE
     func hideKeyboard(){
-        
+    
         view.endEditing(true)
     }
-    
-    //SETS CONTROL FLOW OF TEXT BOXES - AJE
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         let nextTag = textField.tag + 1;
@@ -78,41 +90,16 @@ class SignInViewController: UIViewController, UITextFieldDelegate{
         {
             //CLEAR KEYBOARD AND TRY LOGIN - AJE
             textField.resignFirstResponder()
-            loginSpecifiedUser(email: emailField.text!, password: passwordField.text!)
         }
         return false
     }
 
-    @IBAction func login(_ sender: AnyObject) {
-        
-        loginSpecifiedUser(email: emailField.text!, password: passwordField.text!)
-    }
-    
-    @IBAction func createAccount(_ sender: AnyObject) {
-        
-        print("CREATE ACCOUNT")
-    }
-    
-    func loginSpecifiedUser(email: String, password: String){
-        
-        //VALIDATE EMAIL/PASSWORD AND LOGIN - AJE
-        if(emailField.text == ""){
-            
-            print("Please enter email address.")
-        }else if(passwordField.text == ""){
-            
-            print("Please enter password.")
-        }else{
-        
-            print("LOGGED IN \(email) with password \(password).")
-        }
-        
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
     /*
     // MARK: - Navigation
