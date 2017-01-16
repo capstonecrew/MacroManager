b92061ad1a2f82c66d5b618f5f7dcad137ed6642
Bradley-Cooley <bmcooley@asu.edu>
2017-01-15 22:56:11 -0800

    Populate table view with static search data

--- MacroManager/SearchViewController.swift
+++ MacroManager/SearchViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UITableViewController, UISearchBarDelegate, UISearchDisplayDelegate {
+    
+    @IBOutlet var resultsTableView: UITableView!
+    
+    var foodSearchResults:Array<NixItem> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.navigationBar.barTintColor = UIColor.red
+        
+        performSearch(searchText: "tacos")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +27,33 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func performSearch (searchText: String) {
+        foodSearchResults.removeAll()
+        
+        NixApiManager.search(query: searchText, page: 0) { response in
+            for item in response.value! {
+                print(item.toString())
+                //Fill table view
+                self.foodSearchResults.append(item)
+            }
+            
+            // Refresh table view after new data
+            self.resultsTableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.foodSearchResults.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath)
+        let item = self.foodSearchResults[indexPath.row]
+        
+        cell.textLabel?.text = item.itemName
+        
+        return cell
+    }
     /*
     // MARK: - Navigation
 
