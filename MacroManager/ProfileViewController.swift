e1ade1bcf4a34bd2d19643634ea04ee377cd79df
alexschultzAZ <alexhschultz@gmail.com>
2017-01-18 16:09:30 -0700

    Setup Profile Page
    Partially complete profile page

--- MacroManager/ProfileViewController.swift
+++ MacroManager/ProfileViewController.swift
@@ -4,32 +4,33 @@
 //
 //  Created by Aaron James Edwards on 11/16/16.
 //  Copyright © 2016 Aaron Edwards. All rights reserved.
-//
+//  Alex Schultz - 1/18/17
 
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var userLevelLabel: UILabel!
+    @IBOutlet weak var userBirthField: UITextField!
+    @IBOutlet weak var userGoalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         navigationController?.navigationBar.barTintColor = UIColor(red:0.40, green:0.40, blue:0.40, alpha:1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Coolvetica", size: 23)!, NSForegroundColorAttributeName: UIColor.white]
+        self.navigationItem.title = "Profile"
+        userNameField.text = "Aaron Edwards" // FILLER TEXT FOR NOW TO BE REMOVED
+        userBirthField.text = "11/23/1992"
     }
 
+    @IBAction func changeGoalButton(_ sender: Any) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
