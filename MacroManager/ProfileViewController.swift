15e1660a93e5b7816d6fc63cab980d73e5a7441b
alexschultzAZ <alexhschultz@gmail.com>
2017-01-18 17:04:04 -0700

    Modify Profile Screen

--- MacroManager/ProfileViewController.swift
+++ MacroManager/ProfileViewController.swift
@@ -14,14 +14,18 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var userLevelLabel: UILabel!
     @IBOutlet weak var userBirthField: UITextField!
     @IBOutlet weak var userGoalLabel: UILabel!
+    @IBOutlet weak var userWeightField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.barTintColor = UIColor(red:0.40, green:0.40, blue:0.40, alpha:1.0)
         self.navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Coolvetica", size: 23)!, NSForegroundColorAttributeName: UIColor.white]
         self.navigationItem.title = "Profile"
-        userNameField.text = "Aaron Edwards" // FILLER TEXT FOR NOW TO BE REMOVED
-        userBirthField.text = "11/23/1992"
+        // FILLER TEXT TO BE CHANGED
+        userNameField.text = "Aaron Edwards"
+        userBirthField.text = "24 years old"
+        userWeightField.text = "150 pounds"
+        
     }
 
     @IBAction func changeGoalButton(_ sender: Any) {
