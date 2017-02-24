56e711ae39028a0971a29198480e1f36f2eec58d
Aaron Edwards <aaron.j.edwards@me.com>
2017-02-24 13:02:05 -0700

    Changed placeholder image for favorite foods cell.

--- MacroManager/DashboardViewController.swift
+++ MacroManager/DashboardViewController.swift
@@ -184,7 +184,7 @@ class DashboardViewController: UITableViewController, UICollectionViewDelegate, 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCollectionCell", for: indexPath) as!FoodCollectionCell
         cell.tag = indexPath.row
         cell.foodLbl.text = currentUser.favoriteLog[indexPath.row].itemName
-        cell.foodImageView.af_setImage(withURL: URL(string: currentUser.favoriteLog[indexPath.row].imageUrl!)! , placeholderImage: UIImage(named: "taco_Icon"), filter: CircleFilter())
+        cell.foodImageView.af_setImage(withURL: URL(string: currentUser.favoriteLog[indexPath.row].imageUrl!)! , placeholderImage: UIImage(named: "placeholder"), filter: CircleFilter())
         cell.delegate = self
         
         return cell
